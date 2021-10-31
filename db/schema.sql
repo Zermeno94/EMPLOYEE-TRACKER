a8f0ddfc8a32f178161c43c60e033742f6779aaf
MIRANDA <miranda.zermeno4@gmail.com>
2021-10-30 22:09:21 -0500

    added express listner and middleware to server.js

---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXIST company_db;
+CREATE DATABASE compnay_db;
+
+USE company_db;
--- server.js
+++ server.js
@@ -1,2 +1,15 @@
-// dependencies 
+// Dependencies 
 const inqurier =require ('inqurier');
+const mysql= require('mysql2');
+const consoleTable = require('console.table');
+
+// Connecting to the DB database
+const db= mysql.createConnection(
+    {
+        host:'localhost',
+        user: 'root',
+        password:'',
+        database: 'company_db' 
+    },
+    console.log('Connected to the company_db.')
+);
