7297984c7a4a710f1df6e16222c306b03d5ba177
MIRANDA <miranda.zermeno4@gmail.com>
2021-11-02 19:44:53 -0500

    working on seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,31 @@
 INSERT INTO department (name)
-VALUE ("Management"),
-             ("Sales "),
-             ("Accounting"),
-             ("Warehouse");
+VALUE ("Hokage"),
+            ("Kage"),
+             ("Chuunin "),
+             ("Jounin"),
+             ("Genin");
+
+INSERT INTO role(title,salary,department_id)
+VALUES  ('Hokage', 450000,2),
+                ('Kage',50000,4),
+                ('Chuunin',100000,3)
+                ('Jounin Clerk',40000,1)
+                ('Genin Handler'55000,5)
+
+INSERT INTO employees (first_name,last_name,roles_id,manager_id)
+VALUES  ('Naruto', 'Uzumaki',1,NULL),
+                ( 'Sasuke' , 'Uchiha',2, NULL),
+                ('Itachi', 'Uchiha',3, NULL),
+                ('Kakashi', 'Hatake',4, NULL),
+                ('Hinata', 'Hyuga',5,NULL),
+                ('Pervy','Sage',6,NULL),
+                ('Grandma', 'Tsunade',7,NULL)
+                ('Sakura','Haruno',8,NULL),
+                ('Rock','Lee',9,NULL)
+                ('Ino','Yamanka',10,NULL),
+                ('Neji','Hyuga',11,NULL),
+                ('Kiba', 'Inuzuka',12,NULL),
+                ('Kabuto','Yakushi',13, NULL);
+        
+        -- TODO--
+        -- Need to create role id and manger ids
