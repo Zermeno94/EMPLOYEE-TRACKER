20593f3b70c2de825ac8d61f17e78acd20a31c8f
MIRANDA <miranda.zermeno4@gmail.com>
2022-01-14 18:53:07 -0600

    working on errors

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,13 +15,13 @@ VALUES  ('CEO', 450000,1),
                 ('Sales Representative',55000,5);
 
 INSERT INTO employees (first_name,last_name,roles_id,manager_id)
-VALUES  ('Naruto', 'Uzumaki',1,NULL),
-                ( 'Sasuke' , 'Uchiha',2, 1),
-                ('Itachi', 'Uchiha',3, 2),
-                ('Kakashi', 'Hatake',4, NULL),
-                ('Hinata', 'Hyuga',5,3),
-                ('Pervy','Sage',6,1),
-                ('Grandma', 'Tsunade',0,4),
-                ('Sakura','Haruno',3,5),
-                ('Rock','Lee',4,6);
+VALUES  ('Naruto', 'Uzumaki',1,NULL);
+                -- ( 'Sasuke' , 'Uchiha',2, 1); 
+                -- ('Itachi', 'Uchiha',3, 2),
+                -- ('Kakashi', 'Hatake',4, 4),
+                -- ('Hinata', 'Hyuga',5,3),
+                -- ('Pervy','Sage',6,1),
+                -- ('Grandma', 'Tsunade',0,4),
+                -- ('Sakura','Haruno',3,5),
+                -- ('Rock','Lee',4,6);
         
